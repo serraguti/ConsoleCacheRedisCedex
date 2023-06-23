25a30832d4dfffaf061d3c39c1e74a5c9b9d6515
serraguti1456 <pacoserranox@gmail.com>
2023-06-23 10:36:41 +0200

    Add project files.

---
+++ ConsoleCacheRedisCedex/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleCacheRedisCedex;
+using ConsoleCacheRedisCedex.Models;
+
+Console.WriteLine("Productos Favoritos");
+ServiceCacheRedis service = new ServiceCacheRedis();
+List<Producto> productos = await service.GetProductosFavoritosAsync();
+if (productos == null)
+{
+    Console.WriteLine("No hay favoritos");
+}
+else
+{
+    foreach (Producto prod in productos)
+    {
+        Console.WriteLine(prod.Nombre);
+    }
+}
+Console.WriteLine("Fin de programa");
